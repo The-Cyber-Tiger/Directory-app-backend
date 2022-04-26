eb5229bbd7e23ac5edc8a711eafb2b6cc9be6311
The-Cyber-Tiger <rodrigo.gibran.dev@gmail.com>
2022-04-26 17:19:49 -0500

    in heroku we trust

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 export const config = {
     dbUrl: 'mongodb+srv://admin:movO1j1e9Ha0bJbG@cluster0.kc7c6.mongodb.net/directory-app?retryWrites=true&w=majority',
-    serverPORT: process.env.PORT || '8001',
+    serverPORT: process.env.PORT,
     secrets: {
         jwt: 'moonrock',
         jwtExp: '100d'
